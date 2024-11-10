c30735c9e881579354f74e98d1f7bb6288d83060
WilderWR7 <wilder.mayta91@gmail.com>
2024-11-10 12:36:10 -0400

    redux-configuration

---
+++ src/app/dashboard/favorites/page.tsx
@@ -0,0 +1,18 @@
+import { PokemonGrid } from "@/pokemons";
+import { getPokemons } from "@/pokemons/hooks";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Favoritos',
+    description: 'Listado de pokémons'
+}
+
+export default async function Pokemonspage() {
+    const pokemons = await getPokemons(151, 0);
+    return (
+        <div className="flex flex-col">
+            <span className="text-5xl my-2">Listado de Pokémons <small className="text-blue-500">estático</small></span>
+            <PokemonGrid pokemons={[]} />
+        </div>
+    )
+}
--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,9 +1,17 @@
+import { SimpleWidget } from '@/components'
 import React from 'react'
 
 export default function PageMain() {
     return (
-        <div>
-            <h1>Dashboard Page Main</h1>
+        <div className='text-black p-2'>
+            <h1 className='mt-2 text-3xl'>Dashboard</h1>
+            <span className='text-xl'>Informacion general</span>
+            <div className='flex flex-wrap p-2 items-center justify-center'>
+                <SimpleWidget />
+                <SimpleWidget />
+                <SimpleWidget />
+                <SimpleWidget />
+            </div>
         </div>
     )
 }
